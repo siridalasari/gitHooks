7dcb2fbb7e585b14cc84fc8b6b3429e7e4d2c73e
Sirisha Dalasari <sirishadalasari2006@gmail.com>
2026-03-17 18:08:53 +0530

    adds add function

---
+++ main.js
@@ -0,0 +1,8 @@
+import { add } from "./src/app.js";
+
+const main = () => {
+  const sum = add(2, 4);
+  console.log(sum);
+}
+
+main();
---
+++ test/app_test.js
@@ -0,0 +1,10 @@
+import { describe, it} from "@std/testing/bdd";
+import { assertEquals} from "@std/assert";
+import { add } from "../src/app.js";
+
+describe("add", () =>{
+  it("sum", () => {
+    const sum = add(1,3)
+    assertEquals(sum, 4)
+  })
+})
