aad5c49d9a8360e4b360cde455302df21cf80d0c
Sirisha Dalasari <sirishadalasari2006@gmail.com>
2026-03-17 18:32:11 +0530

    resolves failing test case

--- test/app_test.js
+++ test/app_test.js
@@ -1,10 +1,10 @@
-import { describe, it} from "@std/testing/bdd";
-import { assertEquals} from "@std/assert";
+import { describe, it } from "@std/testing/bdd";
+import { assertEquals } from "@std/assert";
 import { add } from "../src/app.js";
 
-describe("add", () =>{
+describe("add", () => {
   it("sum", () => {
-    const sum = add(1,3)
-    assertEquals(sum, 3)
+    const sum = add(1, 3)
+    assertEquals(sum, 4)
   })
 })
